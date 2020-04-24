d5270faee321625e117201e6850cb9adcf6df428
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-04-24 11:02:52 +0300

    Checked if all elements in an array satisfy a particular condition using loop and every()

---
+++ check-if-all-elements-contains-something.js
@@ -0,0 +1,19 @@
+//6. To Check Whether Every Element in an Array Meets a Condition
+
+let num = [1,2,3,4,5, 0];
+
+for(i = 0; i < num.length; i++) {
+    if (num[i] >= 0) {
+        console.log ("%c All elements in an array are equal to 0 or more", "color:#00A662");
+    }
+}
+
+//The same result using every() method
+
+let num = [1,2,3,4,5, 0];
+let isZeroOrMore = num.every(number => number >= 0);
+
+if(isZeroOrMore) {
+    console.log("%c All elements in an array are equal to 0 or more", "color:#00A662");
+}
+
