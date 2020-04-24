c966c26110d236ff6fe0e8df8a7bdd475942a863
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-04-24 11:12:08 +0300

    Added style to console.log messages

--- check-if-all-elements-contains-something.js
+++ check-if-all-elements-contains-something.js
@@ -9,11 +9,11 @@ for(i = 0; i < num.length; i++) {
 }
 
 //The same result using every() method
-
+let style = "color: #00A662; background: #eee; font-size: 16px";
 let num = [1,2,3,4,5, 0];
 let isZeroOrMore = num.every(number => number >= 0);
 
 if(isZeroOrMore) {
-    console.log("%c All elements in an array are equal to 0 or more", "color:#00A662");
+    console.log("%c All elements in an array are equal to 0 or more", style);
 }
 
