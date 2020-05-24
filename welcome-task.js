74f472e04abf2186e7c22be1efc6069fa5972839
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-05-24 23:06:47 +0300

    Created a function which finds and returns a specific value of the property

---
+++ welcome-task.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function greet(language) {
+    let languageDatabase = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+    if (languageDatabase.hasOwnProperty(language)) {
+        return languageDatabase[language];
+    } else {
+        return languageDatabase.english;
+    }
+}
+
+greet("sgdsgsgsg");
+
